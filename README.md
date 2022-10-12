c2c156fd0a8d0f3149eee23784265181e68d89be
Michael-Altmann <1327148427@qq.com>
2022-10-12 17:37:00 +0800

    Add uid input text, haptic feedback. Now app functions.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FansIndicator
 粉丝数量指示器
+
+# What is it? 是什么？
+The Fans Indicator can easily track your fans quantity and more info after few steps of settings. 
+粉丝数量指示器经过简单的几步设置就可以追踪up主的粉丝数量等数据。
+
+# How to use? 如何使用？
--- app/src/main/java/com/maltmann/fansindicator/MainActivity.kt
+++ app/src/main/java/com/maltmann/fansindicator/MainActivity.kt
@@ -2,11 +2,19 @@ package com.maltmann.fansindicator
 
 import android.os.Build
 import android.os.Bundle
+import android.view.HapticFeedbackConstants
 import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.alibaba.fastjson.JSON
+import com.github.kittinunf.fuel.Fuel
 import com.google.android.material.color.DynamicColors
+import com.google.android.material.textfield.TextInputEditText
+
+//TODO: Check permission everytime when activity starts.
+//TODO: Set delay after get JSON from web. May combine with short and long press the button.
+
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.R)
@@ -18,21 +26,56 @@ class MainActivity : AppCompatActivity() {
         //==============================================
 
         // Enable Dynamic Color to MainActivity
-        DynamicColors.applyIfAvailable(this)
+        DynamicColors.applyToActivityIfAvailable(this)
 
         // Bind textview
         val textView = findViewById<TextView>(R.id.textView)
-        var counter: Int = 0
-        textView.text = counter.toString()
         // Data should be transmit to this textView.text.
 
         // Bind button0
         val button0 = findViewById<Button>(R.id.button0)
+
+        // Bind input textview.
+        val inputTextView = findViewById<TextInputEditText>(R.id.inputTextView)
+
+        val uid = inputTextView.text
+
         // Set on click actions
         button0.setOnClickListener {
-            textView.text = counter++.toString()
-                // TODO(add vibrator)
+
+            // Get fans number from bilbili site.
+            Fuel.get("https://api.bilibili.com/x/relation/stat?vmid=${uid}&jsonp=jsonp")
+                .response { request, response, result ->
+                    println(request)
+                    println(response)
+                    val (bytes) = result
+                    if (bytes != null) {
+                        // Haptic feedback.
+                        button0.performHapticFeedback(HapticFeedbackConstants.CONFIRM)
+
+                        val follower = JSON.parseObject(String(bytes))
+                            .getJSONObject("data")
+                            .getString("follower")
+
+                        textView.text = follower
+
+                        println("[response bytes] ${String(bytes)}")
+                        println(String(bytes))
+                        println(follower)
+
+                    } else {
+                        button0.performHapticFeedback(HapticFeedbackConstants.REJECT)
+                        textView.text = "ERROR"
+                    }
+                }
+
+
 
         }
+
+
     }
+
+
 }
+
